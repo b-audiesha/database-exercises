a25445d36657a363de0b54b00538e8abe2d95f4a
audiesha brattin <a.brattin@gmail.com>
2021-03-30 15:03:19 -0500

    adding sql work

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,12 @@
 USE codeup_test_db;
-DROP TABLE IF EXIST albums;
-CREATE TABLE IF NOT EXIST albums(
+DROP TABLES IF EXISTs albums;
+
+CREATE TABLE IF NOT EXISTs albums(
 id INT unsigned auto_increment not null,
 artist varchar (128),
 name varchar (128),
 release_date SMALLINT  UNSIGNED,
-sales FLOAT(6,2),
-genre VARCHAR(128),,
+    genre VARCHAR(128),
+    sales FLOAT(6,2),
 PRIMARY KEY  (id)
 );
--- transactions_migration.sql
+++ transactions_migration.sql
@@ -2,12 +2,12 @@ USE bank_db;
 DROP TABLE IF EXIST transactions;
 
 CREATE TABLE IF NOT EXIST transactions(
-id INT ,
+id INT  UNSIGNED NOT NULL AUTO_INCREMENT,
 date DATE - format YYYY-MM-DD,
-description VARCHAR(128),
-memo LONGTEXT,
-amount MONEY,
+description VARCHAR(100),
+memo TEXT,
+amount DECIMAL (10,2),
  transaction_successful BOOLEAN,
-    PRIMARY KEY  (id)
+PRIMARY KEY  (id)
     );
 
